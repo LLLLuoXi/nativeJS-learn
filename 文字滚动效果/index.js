4a66c375490336d13d60a014b52b3ebc2299d622
luoxi <2652445066@qq.com>
2021-11-18 01:39:18 +0800

    test

--- 文字滚动效果/index.js
+++ 文字滚动效果/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-10 23:18:47
- * @LastEditTime: 2021-11-10 23:42:28
+ * @LastEditTime: 2021-11-18 01:38:42
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \nativeJS-exercise\文字滚动效果\index.js
@@ -45,3 +45,4 @@ startScoll()
 //     top += 2
 //     ul.scrollTop = top
 // }, 30)
+// test
