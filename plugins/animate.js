989f4ad64f69b61c0590c1394e1703d09f18c610
luoxi <2652445066@qq.com>
2021-11-26 00:03:57 +0800

    文字滚动显示

--- plugins/animate.js
+++ plugins/animate.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-11-17 22:49:43
- * @LastEditTime: 2021-11-18 00:37:15
+ * @LastEditTime: 2021-11-18 22:06:40
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \nativeJS-exercise\plugins\animate.js
@@ -34,70 +34,11 @@ this.myPlugin.Animate = function(option) {
         }
 
     }
-    // this.myPlugin.Animate = function(option) {
-    //     //默认配置
-    //     var defaultOption = {
-    //         duration: 16, //默认间隔时间，单位毫秒
-    //         total: 1000, //默认总时间
-    //         begin: {}, //初始值
-    //         end: {} //终止值
-    //     };
-    //     this.option = myPlugin.mixin(defaultOption, option);
-    //     this.timer = null; //计时器的id
-    //     //运动总次数
-    //     this.number = Math.ceil(this.option.total / this.option.duration);
-    //     //当前运动次数
-    //     this.curNumber = 0;
-    //     //当前状态
-    //     this.curData = myPlugin.clone(this.option.begin);
-    //     //所有属性运动的总距离
-    //     this.distance = {};
-    //     //所有属性每次运动的距离
-    //     this.everyDistance = {};
-    //     for (var prop in this.option.begin) {
-    //         this.distance[prop] = this.option.end[prop] - this.option.begin[prop];
-    //         this.everyDistance[prop] = this.distance[prop] / this.number;
-    //     }
-    // }
-
-
-/**
- * 开始动画
- * @date 2021-11-17
- * @returns {any}
- */
-// this.myPlugin.Animate.prototype.start = function() {
-//         if (this.timer || this.curNumber === this.number) {
-//             return; //如果之前存在计时器，不做处理
-//         }
-//         if (this.option.onstart) {
-//             this.option.onstart.call(that);
-//         }
-//         var that = this;
-//         this.timer = setInterval(function() {
-//             //改变that.curData
-//             that.curNumber++
-//                 for (const prop in that.curDate) {
-//                     if (that.curNumber === that.number) {
-//                         //最后一次运动
-//                         that.curDate[prop] = that.option.end[prop]
-//                     } else {
-//                         that.curDate[prop] += that.everyDistance[prop]
-//                     }
-//                 }
-//             if (that.option.onmove) {
-//                 that.option.onmove.call(that)
-//             }
-//             if (that.curNumber === that.number) {
-//                 that.stop()
-//                 if (that.option.onover) {
-//                     that.option.onover.call(that);
-//                 }
-//             }
-//         }, that.option.duration)
-
-
-//     }
+    /**
+     * 开始动画
+     * @date 2021-11-17
+     * @returns {any}
+     */
 this.myPlugin.Animate.prototype.start = function() {
         if (this.timer || this.curNumber === this.number) {
             return; //如果之前已经存在计时器，则不做任何处理
--- 文字滚动效果/index.js
+++ 文字滚动效果/index.js
@@ -1,14 +1,17 @@
 /*
  * @Author: your name
  * @Date: 2021-11-10 23:18:47
- * @LastEditTime: 2021-11-18 01:38:42
+ * @LastEditTime: 2021-11-18 22:44:55
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \nativeJS-exercise\文字滚动效果\index.js
  */
 var ul = document.querySelector('.left ul')
+var height = 30
+var curTop = 0 // 由于浏览器中的scrollTop取值不精确
     // 复制第一个li
 function cloneFirstLi() {
+    // 深度克隆
     var firstLi = ul.children[0].cloneNode(true)
     ul.appendChild(firstLi)
 }
@@ -22,7 +25,29 @@ function startScoll() {
 
 //滚动一次
 function scroll() {
-    console.log('111');
+    var animate = new myPlugin.Animate({
+        total: 500,
+        begin: {
+            top: curTop
+        },
+        end: {
+            top: curTop + height
+        },
+        onmove: function() {
+            curTop = this.curData.top
+            ul.scrollTop = curTop
+
+        },
+        onover: function() {
+            if (ul.scrollHeight - height === curTop) {
+                // 滚动条回到0
+                curTop = 0;
+                ul.scrollTop = curTop
+            }
+            console.log(curTop, ul.scrollHeight, height);
+        }
+    })
+    animate.start()
 
 }
 startScoll()
